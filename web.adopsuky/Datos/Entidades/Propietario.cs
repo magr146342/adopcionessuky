4450069cf216a76ec7394834033d1394de80f5d8
manolo garcia <manologarcia1725@hotmail.com>
2020-12-29 00:13:46 -0500

    crearbd

---
+++ web.adopsuky/Datos/Entidades/Propietario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web.adopsuky.Datos.Entidades
+{
+    public class Propietario
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Documento { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Apellido { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Direccion { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Celular { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        public string Telefono { get; set; }
+
+        [Display(Name = "Propietario")]
+        public string NombreCompleto => $"{Nombre} {Apellido}";
+
+        [Display(Name = "Propietario")]
+        public string NombreConDoc => $"{Nombre} {Apellido} - {Documento}";
+    }
+}
--- web.adopsuky/Startup.cs
+++ web.adopsuky/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using web.adopsuky.Datos;
 
 namespace web.adopsuky
 {
@@ -24,6 +27,13 @@ namespace web.adopsuky
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                //cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                cfg.UseMySQL(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
